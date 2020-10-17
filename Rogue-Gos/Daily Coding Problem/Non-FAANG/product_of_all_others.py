b6d114343e54d5d53965fcec6d77b2f0f8c3172b
Monica Hong <momo.star@gmail.com>
2020-10-17 04:19:23 -0700

    10/16 algos 2 product_all_others

---
+++ Rogue-Gos/Daily Coding Problem/Non-FAANG/product_of_all_others.py
@@ -0,0 +1,22 @@
+# This problem was asked by Uber.
+
+# Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+# For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+# Follow-up: what if you can't use division?
+import math
+
+
+def product_all_others(arr_ints):
+    new_array = []
+    for num in range(len(arr_ints)):
+        new_array.append(int(math.prod(arr_ints) / arr_ints[num]))
+        print(new_array)
+    return new_array
+
+yes = product_all_others([1, 2, 3, 4, 5])
+print(yes)
+
+yarp = product_all_others([3, 2, 1])
+print(yarp)
