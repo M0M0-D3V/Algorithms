23d9fbd29cd4408d7fe8dafa4167895019ddd619
Monica Hong <momo.star@gmail.com>
2020-10-20 02:02:35 -0700

    update

--- Rogue-Gos/Daily Coding Problem/Non-FAANG/product_of_all_others.py
+++ Rogue-Gos/Daily Coding Problem/Non-FAANG/product_of_all_others.py
@@ -20,3 +20,33 @@ print(yes)
 
 yarp = product_all_others([3, 2, 1])
 print(yarp)
+
+
+def product_all_others(nums):
+    # generate prefix products
+    prefix_products = []
+    for num in nums:
+        if prefix_products:
+            prefix_products(prefix_products[-1] * num)
+        else:
+            prefix_products.append(num)
+
+    # Generate suffix products
+    suffix_products = []
+    for num in reversed(nums):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1] * num)
+        else:
+            suffix_products(num)
+    suffix_products = list(reversed(suffix_products))
+
+    # Generate result from the product of prefixes and suffixes
+    result = []
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suffix_products[i + 1])
+        elif i == len(nums) - 1:
+            result.append(prefix_products[i - 1])
+        else:
+            result.append(prefix_product[i - 1] * suffix_products[i + 1])
+    return result
