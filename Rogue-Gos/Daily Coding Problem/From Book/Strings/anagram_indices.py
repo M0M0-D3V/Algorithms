a9fada4db794568f34ecaa33bc501af8983f36ea
Monica Hong <momo.star@gmail.com>
2020-10-24 18:01:57 -0700

    algos 10/24

--- Rogue-Gos/Daily Coding Problem/From Book/Strings/anagram_indices.py
+++ Rogue-Gos/Daily Coding Problem/From Book/Strings/anagram_indices.py
@@ -2,6 +2,10 @@
 # For example, given w is ab and s is abxaba, return [0, 3, 4].
 
 
+from collections import defaultdict
+from collections import Counter
+
+
 def anagram_indices(w, s):
     locations = []
     temp_words = []
@@ -26,9 +30,9 @@ def anagram_indices(w, s):
                 else:
                     hashT[temp[j]] = 1
                 print(hashT)
-            
+
             # make a for loop for in hashW and check if T is there and matching values, if not matching, then do not append
-           
+
         #    if hashT == hashW:
         #         locations.append(i)
             # temp_words.append(s[i: i + length])
@@ -39,3 +43,56 @@ def anagram_indices(w, s):
 
 yes = anagram_indices("ab", "abxaba")
 print(f'yes is {yes}')
+
+# brute force method from book
+
+
+def is_anagram(s1, s2):
+    return Counter(s1) == Counter(s2)
+
+
+def anagram_indices(word, s):
+    result = []
+    for i in range(len(s) - len(word) + 1):
+        window = s[i:i + len(word)]
+        if is_anagram(window, word):
+            result.append(i)
+    return result
+
+# the above takes O(w x s)
+
+
+# better one from book
+
+
+def del_if_zero(dict, char):
+    if dict[char] == 0:
+        del dict[char]
+
+
+def anagram_indices(word, s):
+    result = []
+
+    freq = defaultdict(int)
+    for char in word:
+        freq[char] += 1
+
+    for char in s[:len(word)]:
+        freq[char] -= 1
+        del_if_zero(freq, char)
+
+    if not freq:
+        result.append(0)
+
+    for i in range(len(word), len(s)):
+        start_char, end_char = s[i - len(word)], s[i]
+        freq[start_char] += 1
+        del_if_zero(freq, start_char)
+
+        if not freq:
+            beginning_index = i - len(word) + 1
+            result.append(beginning_index)
+
+    return result
+
+# this one runs in O(s) time and space
---
+++ Rogue-Gos/Daily Coding Problem/From Book/Strings/palindrome_pairs.py
@@ -0,0 +1,9 @@
+# Given a list of words, find all pairs of unique indices such that the concatenation of the two words is a palindrome
+
+# for example: given the list ["code", "edoc", "da", "d"], return [(0,1), (1, 0), (2, 3)]
+
+
+def palindrome_pairs(list_words):
+    answer_list = []
+
+    return answer_list
