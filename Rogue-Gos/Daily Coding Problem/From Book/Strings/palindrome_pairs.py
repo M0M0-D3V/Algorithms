c20c33e6c0463723b73da73961fde23596eea4c7
Monica Hong <momo.star@gmail.com>
2020-10-25 21:45:51 -0700

    algo 10/25

--- Rogue-Gos/Daily Coding Problem/From Book/Strings/palindrome_pairs.py
+++ Rogue-Gos/Daily Coding Problem/From Book/Strings/palindrome_pairs.py
@@ -3,7 +3,12 @@
 # for example: given the list ["code", "edoc", "da", "d"], return [(0,1), (1, 0), (2, 3)]
 
 
+def is_palindrome(string1):
+
+
 def palindrome_pairs(list_words):
     answer_list = []
-
+    # make temp list to concatenate
+    for i in range(len(list_words) - 1):
+        for j in range()
     return answer_list
