325e429a33a7700c1af942ebd47b025687a58cec
Monica Hong <momo.star@gmail.com>
2020-10-26 11:40:39 -0700

    algo 10/26

---
+++ Rogue-Gos/Daily Coding Problem/From Book/Strings/smallest_rotated_str.py
@@ -0,0 +1,5 @@
+# Given a string of length n and an integer k. The string can be manipulated by taking one of the first k letters and moving it to the end of the string.
+
+# Write a program to determine the lexicographically smallest string that can be created after an unlimited number of moves
+
+# For example, suppose we are given the string 'daily' and k = 1. The best we can create in this case is ailyd
--- Rogue-Gos/Daily Coding Problem/From Book/Strings/zigzag.py
+++ Rogue-Gos/Daily Coding Problem/From Book/Strings/zigzag.py
@@ -1,1 +1,32 @@
 # Given a string and a number of lines k, print the string in zigzag form. In zigzag, characters are printed out diagonally from top left to bottom right until reaching the kth line, then back up to top right, and so on
+
+# book solution
+
+
+def get_spaces(row, desc, k):
+    max_spaces = (k - 1) * 2 - 1
+    if desc:
+        spaces = max_spaces - row * 2
+    else:
+        spaces = max_spaces - (k - 1 - row) * 2
+    return spaces
+
+
+def is_descending(index, k):
+    return index % (2 * (k - 1)) < k - 1
+
+
+def zigzag(sentence, k):
+    n = len(sentence)
+
+    for row in range(k):
+        i = row
+        line = [" " for _ in range(n)]
+
+        while i < n:
+            line[i] = sentence[i]
+            desc = is_descending(i, k)
+            spaces = get_spaces(row, desc, k)
+            i += spaces + 1
+
+        print("".join(line))
