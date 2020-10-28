3a7f646da763941dbab686d4bfa464c27db1feb0
Monica Hong <momo.star@gmail.com>
2020-10-28 00:58:51 -0700

    algo 10/27

---
+++ Rogue-Gos/Codewars/5-Kyu/Working/MaximumSubarraySum.py
@@ -0,0 +1,18 @@
+# The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+# maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+# // should be 6: [4, -1, 2, 1]
+# Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+# Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+
+def max_sequence(arr):
+    max_ending_here = max_so_far = 0
+    for x in arr:
+        max_ending_here = max(x, max_ending_here + x)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+
+
+print(max_sequence([2, 1, 3, 4, -1, 2, 1, -5, 4]))
