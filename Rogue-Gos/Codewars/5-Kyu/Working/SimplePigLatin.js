7642d3c75e26e4f0b472b77c6b7e30ab326fc0ab
Monica Hong <momo.star@gmail.com>
2020-11-01 04:13:28 -0800

    11/1 algo simplepiglatin

--- Rogue-Gos/Codewars/5-Kyu/Working/SimplePigLatin.js
+++ Rogue-Gos/Codewars/5-Kyu/Working/SimplePigLatin.js
@@ -4,24 +4,76 @@
 // pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 // pigIt('Hello world !');     // elloHay orldway !
 
+// logic points:
+// [x] split the string into substrings separated by space
+// [x] loop through each substring and move first char to last position
+// [x] find a way to detech non alphanumeric chars
+// [x] once discovered, record the value and position in the substring
+// [x] find a way to clean each substring from special characters
+// [x] if special char is at end of substring, just pop
+// [x] if not, move it to end of substring by swapping and then pop
+// [] add back the special characters from hashT
+
 function pigIt(str) {
     let strArr = str.split(" ")
-    console.log(strArr);
-    let what = strArr.map(i => {
-        Ay(i)
-    }).join(" ")
-    console.log(what);
-    return what;
+    let theSpecials = {}
+    for(char of str) {
+        if(hasSpecial(char)) {
+            theSpecials[str.indexOf(char)] = char
+            console.log(theSpecials)
+        }
+    }
+    let temp = []
+    for(let i = 0; i < strArr.length; i++) {
+        temp.push(Ay(strArr[i]))
+    }
+    return temp.join(" ");
+
 }
 
 function Ay(word) {
-    let splitword = word.split("");
-    let first = splitword.shift();
-    console.log(splitword);
-    console.log(`first is ${first}`)
-    let wordAy = splitword.push(first);
-    console.log(wordAy.toString());
+    let cleanWord = findSpecials(word)
+    if(cleanWord) {
+        let splitword = cleanWord.split("");
+        let first = splitword.shift();
+        splitword.push(first + "ay");
+        return splitword.join("")
+    }
     return word
 }
 
-console.log(pigIt("what will happen?"))
+
+function findSpecials(word) {
+    let tempWord = word
+    for (let i = 0; i < word.length; i++) {
+        if(hasSpecial(word[i])) {
+            if(i == tempWord.length - 1) {
+                tempWord = tempWord.split("")
+                tempWord.pop()
+                tempWord = tempWord.join("")
+            }
+            else {
+                removeSpecial(tempWord, tempWord.indexOf(tempWord[i]))
+            }
+        }
+    }
+    return tempWord
+}
+
+function hasSpecial(str){
+    return /[~`!#$%\^&*+=\-\[\]\\';,./{}|\\":<>\?]/g.test(str);
+}
+
+function removeSpecial(word, index) {
+    console.log("we are in removeSpecial f(x)")
+    let temp = word[index]
+    for(let i = index; i < word.length - 1;i++) {
+        word[i] = word[i + 1]
+    }
+    word[word.length - 1] = temp
+    return word
+}
+
+let wow = hasSpecial(".")
+console.log(wow)
+console.log(pigIt("what will happen dfd meow  ?"))
