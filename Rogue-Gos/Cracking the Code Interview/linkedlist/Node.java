da718285f1a97d210e323b1d7a8a135c1fc602a7
Monica Hong <momo.star@gmail.com>
2021-02-18 09:21:31 -0800

    algo 2/18

---
+++ Rogue-Gos/Cracking the Code Interview/linkedlist/Node.java
@@ -0,0 +1,51 @@
+package linkedlist;
+
+public class Node {
+    Node next = null;
+    int data;
+
+    public Node(int d) {
+        data = d;
+    }
+
+    void appendToTail(int d) {
+        Node end = new Node(d);
+        Node n = this;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    Node deleteNode(Node head, int d) {
+        if (head == null)
+            return null;
+        Node n = head;
+
+        if (n.data == d) {
+            return head.next;
+        }
+
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return head;
+    }
+
+    void removeDupes(Node n) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        Node previous = null;
+        while(node.next != null) {
+            if(set.contains(n.data)) {
+                previous.next = n.next;
+            } else {
+                set.add(n.data);
+                previous = n;
+            }
+            n = n.next;
+        }
+}
