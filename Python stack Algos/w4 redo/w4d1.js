d5da97e5256dc00111fcb149b8f0352991f3a364
momoxstarx <momo.star@gmail.com>
2021-11-06 03:05:18 -0700

    alg w6

---
+++ Python stack Algos/w4 redo/w4d1.js
@@ -0,0 +1,55 @@
+// Recursion
+
+// Base case(s)
+// Forward movement
+
+// return value or function call
+// Call Stack
+// Pass by value/reference
+
+/* ******************************************************************************** */
+
+/*
+  Recursively sum an arr of ints
+*/
+
+// const nums1 = [1, 2, 3]
+// const expected1 = 6
+
+// add params if needed for recursion
+
+function sumArr(nums, sum = 0) {
+  // code here
+  if(nums.length < 1) {
+    return sum
+  }
+  sum += nums.shift()
+  return sumArr(nums, sum)
+}
+console.log(sumArr([1, 2, 3]))
+/* ******************************************************************************** */
+
+/*
+        Recursive Sigma
+      
+        Input: integer
+        Output: sum of integers from 1 to Input integer
+      
+      */
+
+// const num1 = 5
+// const expected1 = 15
+// // Explanation: (1+2+3+4+5)
+
+// const num2 = 2.5
+// const expected2 = 3
+// // Explanation: (1+2)
+
+// const num3 = -1
+// const expected3 = 0
+
+function recursiveSigma(n) {
+  // code here
+  n = Math.floor(n)
+  
+}
---
+++ Python stack Algos/w4 redo/w4d4.js
@@ -0,0 +1,53 @@
+/*
+  Sum To One Digit
+  Implement a function sumToOne(num)​ that,
+  given a number, sums that number’s digits
+  repeatedly until the sum is only one digit. Return
+  that final one digit result.
+
+  Tips:
+    to access digits from a number, need to convert it .toString() to access each digit via index
+    parseInt(arg) returns arg parsed as an integer, or NaN if it couldn't be converted to an int
+    isNaN(arg) used to check if something is NaN
+*/
+
+// const num1 = 5
+// const expected1 = 5
+
+// const num2 = 10  => 1
+// const expected2 = 1
+
+// const num3 = 25  => 7
+// const expected3 = 7
+
+// const num4 = 25761  => 21  => 3
+// // 21
+// // 3
+// const expected4 = 3
+
+function sumToOneDigit(num) {
+  // code here
+}
+
+/*****************************************************************************/
+
+/*
+    String Anagrams
+  
+    Given a string,
+    return array where each element is a string representing a different anagram (a different sequence of the letters in that string).
+  // methods like yseterday: substring, slice...
+    examples of anagram: cat => tac => act => tca
+    sdlfhsjdfh => ldfhsjdfhs => dfhsjdfhsl => fhshfhsld => jdhsfshld
+    Ok to use built in methods
+  */
+// total length of array should be = length * (length-1)
+// const str11 = "lim";
+// const expected11 = ["ilm", "iml", "lim", "lmi", "mil", "mli"];
+// Order of the output array does not matter
+
+// const str12 = "abra"  => 4 * 3 = 12 length
+
+function generateAnagrams(str, partial = "", arr = []) {
+ 
+}
